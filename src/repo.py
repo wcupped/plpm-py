1a708de386081b9205bf7ffe48b66a5b3ceb194f
Nikita <nikitabebradude@gmail.com>
2026-02-15 00:18:10 +0300

    Hotfix

--- src/repo.py
+++ src/repo.py
@@ -1,12 +1,14 @@
 import json
 import requests
 from config import BASE_DIR, REPO_FILE, HASHES_FILE, REPO_URL, HASHES_URL
-
+import os
 
 def sync_repo():
     BASE_DIR.mkdir(parents=True, exist_ok=True)
 
     print("Syncing repository...")
+    if REPO_FILE.exists():
+        os.remove(REPO_FILE)
     with requests.get(REPO_URL, stream=True) as req:
         req.raise_for_status()
         with open(REPO_FILE, "wb") as file:
@@ -15,6 +17,8 @@ def sync_repo():
                     file.write(chunk)
 
     print("Syncing hashes...")
+    if HASHES_FILE.exists():
+        os.remove(HASHES_FILE)
     with requests.get(HASHES_URL, stream=True) as req:
         req.raise_for_status()
         with open(HASHES_FILE, "wb") as file:
